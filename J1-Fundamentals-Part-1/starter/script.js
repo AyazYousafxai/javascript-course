c41c758de71867983170f92b43cf65f4c7ddede9
AyazYousafxai <ayazpk6630@gmail.com>
2022-03-29 22:11:14 +0500

    javascript lecture

---
+++ J1-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+// let js = "wonderful";
+// if (js==="wonderful") alert("pass java condition")
+// //math func 
+// console.log(34*32+3-6)
+
+let javascriptDataType = true;
+console.log(javascriptDataType);
+
+console.log(typeof true);
+console.log(typeof javascriptDataType);
+console.log(typeof 4);
+console.log(typeof true);
+
+let checkVariable;
+console.log(checkVariable)
+console.log(typeof checkVariable);
+
+checkVariable = 3;
+console.log(typeof checkVariable);
+console.log(typeof null);
+
+//  let and cons introduce in ES6 and var keyword is the old way to declare variable (ES5)
+
+// let can be change or resign
+let check = true;
+check = false;
+
+// on the other hand const variable can not be change
+const brandName = "everything5pond";
+// it will show error
+brandName = "hello"; 
+// var is function scop
+var check1=true;
+check1=false;
